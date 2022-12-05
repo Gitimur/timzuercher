35851ceaa2e26c0ff13c05d8e0052fdbab9370fe
Simi2394 <simon.perdrizat@students.bfh.ch>
2022-12-05 22:37:40 +0100

    Update script.js
    Einbindung der Funktion für das Kontaktformular und dessen Funktion.

--- script.js
+++ script.js
@@ -24,3 +24,47 @@ function expandNav() {
   }
 }
 
+/*---------Anpassungen von simi2394 vom 05.12.2022-----------*/
+/*---------Funktion für Kontaktformular Responses----------*/
+const form = document.getElementById('form');
+const msg = document.getElementById('msg');
+
+form.addEventListener('submit', function(e) {
+    const formData = new FormData(form);
+    e.preventDefault();
+    var object = {};
+    formData.forEach((value, key) => {
+        object[key] = value
+    });
+    var json = JSON.stringify(object);
+    msg.innerHTML = "Bitte warten..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                msg.innerHTML = json.message;
+            } else {
+                console.log(response);
+                msg.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            msg.innerHTML = "Da ging etwas schief.";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                msg.innerHTML = "";
+            }, 5000);
+        });
+});
+
